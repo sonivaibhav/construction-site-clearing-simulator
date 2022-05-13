2cea97eb2610428f7aaef59aab099395bd899b55
Vaibhav Soni <sonivaibhav332@gmail.com>
2022-05-13 11:52:01 -0400

    Cleaned up basic app skeleton
    - Removed default HTML/SCSS provided by Angular CLI.
    - Updated app title.
    - Adjusted respective unit test cases for `app.component.ts`.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'construction-site-clearing-simulator'`, () => {
+  it(`should have as title 'construction site clearing simulator'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('construction-site-clearing-simulator');
+    expect(app.title).toEqual('construction site clearing simulator');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('construction-site-clearing-simulator app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Construction Site Clearing Simulator app is running!');
   });
 });
